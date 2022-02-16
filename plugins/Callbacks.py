837ef15feeafa82b8adcd74cc04621bce589521b
FUCK OFF ANE <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-02-16 13:13:03 +0530

    Create Callbacks.py

---
+++ plugins/Callbacks.py
@@ -0,0 +1,15 @@
+ait query.message.edit_text(
+            tennxt="▢▢bn▢
+        )bnbw
+
+        await query.mbbnessage.edit_text(
+            text="▣▢▢"
+        )
+
+        await nbbquery.message.edit_text(
+            text="▣▣▢"
+        )
+
+        await query.message.edit_text(
+            text="▣▣▣"
+        )
